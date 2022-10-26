b810e4554ade1f9d16568e315632d777a92fbce7
0xchang <oxchang@163.com>
2022-10-26 16:50:37 +0800

    src

--- bot2/plugins/help/helpmenu.py
+++ bot2/plugins/help/helpmenu.py
@@ -21,8 +21,8 @@ async def helpmenu_handle(event:Event):
 /关注详情    查看关注主播详情
 /测试超管    测试超管权限
 /测试权限    测试群成员权限
-/开启动态    开启动态推送
-/关闭动态    关闭动态推送
+/开启动态uid 开启动态推送
+/关闭动态uid 关闭动态推送
 /开启全体动态 开启动态推送at全体成员
 /关闭全体动态 关闭动态at全体成员
 /开启直播uid 开启直播推送
@@ -38,5 +38,7 @@ async def helpmenu_handle(event:Event):
 /舔狗日记    获取一条舔狗日记(3秒)
 /随机语录    获取一跳随机语录(3秒)
 /随机数 x    获取x个随机数(3秒)
-/随机表情    获取一个随机表情'''
+/随机表情    获取一个随机表情
+/抽老婆      抽一个老婆送给你
+/抽歌手      抽一个歌手'''
     await helpmenu.finish(Message(f'[CQ:at,qq={event.get_user_id()}]%s'%help_detail))
