b437978a88c21fff1faf68d331a0b73899583097
0xchang <oxchang@163.com>
2022-11-07 15:55:25 +0800

    src

--- bot2/plugins/xiuxian/helpxiuxian.py
+++ bot2/plugins/xiuxian/helpxiuxian.py
@@ -15,32 +15,32 @@ from nonebot.adapters.onebot.v11.message import Message
 
 def create_xiuxian_help():
     helpxiuxian='''
-    修仙帮助----------获取帮助
-    /查询--------------查看个人信息
-    进化---------------消耗灵石增强属性
-    大进化-------------比进化牛必
-    究极进化----------比大进化牛必
-    签到---------------签到得奖励
-    /改名--------------改名字
-    /改性别-----------改变性别
-    打工---------------赚钱
-    钓鱼---------------赚钱得经验
-    回复---------------消耗灵石回HP,MP
-    大回复术----------消耗大量灵石回复HP,MP
-    超级回复----------反正比大回复术还nb
-    练功---------------你可以练功
-    /清零--------------直接把xx数据清零(管理员)
-    /闭关x------------闭关x分钟，期间不能干任何事
-    /杀人x------------攻击十次x
-    打怪---------------随机打怪，不强的时候别来
-    /打bossxx-------打xxboss，boss很强，别来
-    查boss-----------查看boss列表,boss三十分钟复活一次
+    修仙帮助------获取帮助
+    /查询--------查看个人信息
+    进化---------消耗灵石增强属性
+    大进化--------比进化牛必
+    究极进化------比大进化牛必
+    签到---------签到得奖励
+    /改名--------改名字
+    /改性别-------改变性别
+    打工---------赚钱
+    钓鱼---------赚钱得经验
+    回复---------消耗灵石回HP,MP
+    大回复术------消耗大量灵石回复HP,MP
+    超级回复------反正比大回复术还nb
+    练功---------你可以练功
+    /清零--------直接把xx数据清零(管理员)
+    /闭关x--------闭关x分钟，期间不能干任何事
+    /杀人x--------攻击十次x
+    打怪---------随机打怪，不强的时候别来
+    /打bossxx------打xxboss，boss很强，别来
+    查boss--------查看boss列表,boss三十分钟复活一次
     '''
     lino_path=os.path.join(os.getcwd(),'img','lino.jpg')
     current=Image.open(lino_path)
     draw=ImageDraw.Draw(current)
-    font=ImageFont.truetype(os.path.join(os.getcwd(),'fonts','syht_CN-Heavy.otf'),size=40)
-    draw.text((10, 420),text=helpxiuxian,font=font,fill=(48,128,20))
+    font=ImageFont.truetype(os.path.join(os.getcwd(),'fonts','千图小兔体.ttf'),size=40)
+    draw.text((10, 420),text=helpxiuxian,font=font,fill=(94,38,18))
     current.save('./img/helpxiuxian.png')
 
 create_xiuxian_help()
