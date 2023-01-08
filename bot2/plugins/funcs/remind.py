7c276f71fc83385e337ca815b4cb5b41270cf9a2
0xchang <oxchang@163.com>
2023-01-08 13:00:27 +0800

    src

--- bot2/plugins/funcs/remind.py
+++ bot2/plugins/funcs/remind.py
@@ -12,14 +12,16 @@ from nonebot.adapters.onebot.v11 import GroupMessageEvent
 from nonebot import on_keyword
 import asyncio
 
-remind1=on_keyword('提醒',priority=100,block=False)
+remind1 = on_keyword({'提醒'}, priority=100, block=False)
+
+
 @remind1.handle()
-async def _(event:GroupMessageEvent):
-    mess=event.get_plaintext()
-    mess=mess.split('提醒')
-    if (not mess[0].isdigit()) or mess[1]=='':
+async def _(event: GroupMessageEvent):
+    mess = event.get_plaintext()
+    mess = mess.split('提醒')
+    if (not mess[0].isdigit()) or mess[1] == '':
         await remind1.finish(Message('格式不对，请输入时间 提醒 事件,例如30 提醒 记得吃饭(30分钟后提醒吃饭)'))
-    t=int(mess[0].strip())
+    t = int(mess[0].strip())
     await remind1.send(Message('已添加到提醒事项中!'))
-    await asyncio.sleep(t*60)
-    await remind1.finish(Message(f'\n提醒通知:\n记得{mess[1]}哦!'),at_sender=True)
+    await asyncio.sleep(t * 60)
+    await remind1.finish(Message(f'\n提醒通知:\n记得{mess[1]}哦!'), at_sender=True)
